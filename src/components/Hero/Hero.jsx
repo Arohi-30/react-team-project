177b3b9b68b54dce4dadfbe2eada6e9d9aca873c
Arohi-30 <arohimehta300703@gmail.com>
2025-11-04 22:48:53 +0530

    Completed Hero Page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
                 </div>
 
                 <div  className="flexColStart hero-des">
-                    <span>Find a variety of properties that suit you very easity</span>
-                    <span>Forget all difficulties in finding a residence for you</span>
+                    <span className='secondaryText'>Find a variety of properties that suit you very easity</span>
+                    <span className='secondaryText'>Forget all difficulties in finding a residence for you</span>
                 </div>
 
                 <div className="flexCenter search-bar">
@@ -30,29 +30,30 @@ const Hero = () => {
                 </div>
 
                 <div className="flexCenter stats">
-                    <div className="flexColStart stat">
+                    <div className="flexColCenter stat">
                         <span>
                             <CountUp start={8800} end={9000} duration={4}/>
                             <span>+</span>
-                            </span><span>
+                            </span>
+                            <span span className='secondaryText'>
                                 Premium Products
                             </span>
                     </div>
 
-                    <div className="flexColStart stat">
+                    <div className="flexColCenter stat">
                         <span>
                             <CountUp start={1950} end={2000} duration={4}/>
                             <span>+</span>
-                            </span><span>
+                            </span><span span className='secondaryText'>
                                 Happy Customers
                             </span>
                     </div>
 
-                    <div className="flexColStart stat">
+                    <div className="flexColCenter stat">
                         <span>
                             <CountUp end={28}/>
                             <span>+</span>
-                            </span><span>
+                            </span><span span className='secondaryText'>
                                 Award Winning
                             </span>
                     </div>

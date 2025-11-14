b6898422043999f3a54e554cef6fdf0738159db4
parshva04 <parshvas1104@gmail.com>
2025-11-14 22:33:53 +0530

    Made some Components Reusable and Removed unnecessary things

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
-        <img src="./logo.png" alt="" width={100} />
+        <Link to="/">
+          <img src="./logo.png" alt="" width={100} />
+        </Link>
 
         <div className="flexCenter h-menu">
-          <a href="">Residencies</a>
-          <a href="">Our Values</a>
-          <a href="">Conatact</a>
-          <a href="">Get Started</a>
-          <button className= "button">
-            <a href="">Contact</a>
-          </button>
+          <NavLink to="/properties">Properties</NavLink>
           
+            <a href="mailto:homzyrealestate@gmail.com">Contact</a>
+         
+          <button className="button">
+            Login
+          </button>
         </div>
       </div>
     </section>

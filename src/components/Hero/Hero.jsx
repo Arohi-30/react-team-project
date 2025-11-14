097353d2907750d9154dc12e1bf7c6364b68948e
parshva04 <parshvas1104@gmail.com>
2025-11-14 17:19:37 +0530

    Added animation on Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import {HiLocationMarker} from 'react-icons/hi'
 import CountUp from 'react-countup';
+import {motion} from 'framer-motion'
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -12,10 +13,17 @@ const Hero = () => {
                     <div className="orange-circle">
 
                     </div>
-                    <h1>
+                    <motion.h1 
+                    initial={{y:"2rem" , opacity : 0}}
+                    animate={{y:0 , opacity:1}}
+                    transition={{
+                        duration:2,
+                        type:"spring"
+                    }}
+                    >
                         Discover <br/> 
                         Most Suitable <br/> Property   
-                    </h1>
+                    </motion.h1>
                 </div>
 
                 <div  className="flexColStart hero-des">
@@ -62,9 +70,16 @@ const Hero = () => {
             </div>
 
             <div className="flexCenter hero-right">
-                <div className="image-container">
+                <motion.div 
+                initial={{x:"7rem",opacity:0}}
+                animate={{x:"0",opacity:1}}
+                transition={{
+                    duration:2,
+                    type:"spring"
+                }}
+                className="image-container">
                     <img src="./hero-image.png" alt="" />
-                </div>
+                </motion.div>
             </div>
         </div>
     </section>

26c8ae038f4c4196944e007b79c1b18e25cb1b37
Arohi-30 <arohimehta300703@gmail.com>
2025-11-17 22:18:54 +0530

    Working on Property Page

--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import SearchBar from '../../components/SearchBar/SearchBar'
+import './Properties.css'
+import useProperties from '../../hooks/useProperties'
 const Properties = () => {
+  const{data, isError, isLoading} = useProperties()
+  console.log(data)
   return (
     <div className="wrapper">
-        properties
+        <div className="flexColCenter paddings innerWidth properties-container">
+          <SearchBar/>
+        </div>
     </div>
   )
 }

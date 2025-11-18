b040adfc9aa599faf2aa7c141b3d45e9e768239f
Prathmesh Mane <prathmesh.mane@mitwpu.edu.in>
2025-11-18 11:33:21 +0530

    Made website responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Header.css'
 import { Link, NavLink } from 'react-router-dom';
+import { BiMenuAltRight } from "react-icons/bi";
+import { getMenuStyles } from "../../utils/common";
+import OutsideClickHandler from "react-outside-click-handler";
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
         <Link to="/">
           <img src="./logo.png" alt="" width={100} />
         </Link>
-
-        <div className="flexCenter h-menu">
+         <OutsideClickHandler
+          onOutsideClick={() => {
+            setMenuOpened(false);
+          }}
+        >
+        <div className="flexCenter h-menu"
+        style={getMenuStyles(menuOpened)}
+        >
           <NavLink to="/properties">Properties</NavLink>
           
             <a href="mailto:homzyrealestate@gmail.com">Contact</a>
@@ -18,7 +28,15 @@ const Header = () => {
             Login
           </button>
         </div>
+         </OutsideClickHandler>
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
+        </div>
       </div>
+      
     </section>
   );
 }
